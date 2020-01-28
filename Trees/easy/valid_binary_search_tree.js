2fa88ca57d9c451a200e15e62639c9e70f661fdf
github username <josephtrettevik@gmail.com>
2020-01-28 13:41:45 -0800

    added recursive solution to Is Binary Search Tree Valid

---
+++ Trees/easy/valid_binary_search_tree.js
@@ -0,0 +1,57 @@
+// Given a binary tree, determine if it is a valid binary search tree (BST).
+
+// Assume a BST is defined as follows:
+
+// The left subtree of a node contains only nodes with keys less than the node's key.
+// The right subtree of a node contains only nodes with keys greater than the node's key.
+// Both the left and right subtrees must also be binary search trees.
+ 
+
+// Example 1:
+
+//     2
+//    / \
+//   1   3
+
+// Input: [2,1,3]
+// Output: true
+// Example 2:
+
+//     5
+//    / \
+//   1   4
+//      / \
+//     3   6
+
+// Input: [5,1,4,null,null,3,6]
+// Output: false
+// Explanation: The root node's value is 5 but its right child's value is 4.
+
+/* ******************************************************************* */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+// @param {TreeNode} root
+// @return {boolean}
+var isValidBST = function(root) {
+    return isValidHelper(root, null, null);
+};
+
+var isValidHelper = function(node, lower, upper) {
+    if (node == null) return true;
+    
+    let val = node.val;
+    if (lower != null && val <= lower) return false;
+    if (upper != null && val >= upper) return false;
+
+    if (!isValidHelper(node.right, val, upper)) return false;
+    if (!isValidHelper(node.left, lower, val)) return false;
+    return true;
+}
+
